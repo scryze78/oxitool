50b8c6825515368b8e534e737a2b1f90c498f0a1
otherpenguin <73692925+otherpenguin@users.noreply.github.com>
2020-10-30 16:43:05 +0100

    Update scan_vuln.py

--- scan_vuln.py
+++ scan_vuln.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
-# fichier: scanDeVulnerabilites.py
+# fichier: scan_vuln.py
 # auteur: Adrien Cellier
 # entreprise: POP School
 # date: 30/10/2020
 
+"""Ce module cherche les vulnérabilités à la propagation d'un ransomware sur un système.
+Il est prévu pour permettre à oxitool.py d'en appeler scan_et_envoi_resultats() ou les fonctions séparément.
+Si il est lancé en programme principal, c'est la fonction main() qui sera executée.
+
+Les fonctions de scan_sV nécessitent d'installer nmap sur le système et d'importer nmap3.
+"""
 # Nécessite nmap sur le système.
 
 # pip3 install python3-nmap
@@ -12,12 +18,15 @@ import nmap3
 # pip3 install getmac
 import getmac
 
-
 # import module_bdd.py
 
 
-def scan(host):
-    # results = nmap3.Nmap().nmap_version_detection(host)
+def scan_sV(host):
+    results = nmap3.Nmap().nmap_version_detection(host)
+    return results
+
+
+def fake_scan_sV(host):
     results = [{'protocol': 'tcp', 'port': '80', 'state': 'open', 'reason': 'syn-ack', 'reason_ttl': '128',
                 'service': {'name': 'http', 'product': 'Microsoft IIS httpd', 'version': '10.0', 'ostype': 'Windows',
                             'method': 'probed', 'conf': '10'}, 'cpe': [{'cpe': 'cpe:/o:microsoft:windows'}]},
@@ -64,14 +73,19 @@ def affichage_resultats(results, mac):
               i.get('cpe', [{}])[0].get('cpe', '0'))
 
 
+def scan_et_envoi_resultats():
+    host = "127.0.0.1"
+    mac = getmac.get_mac_address()
+    results = scan_sV(host)
+    envoi_resultats(results, mac)
+
+
 def main():
     host = "127.0.0.1"
     mac = getmac.get_mac_address()
-    results = scan(host)
+    results = scan_sV(host)
     affichage_resultats(results, mac)
 
 
 if __name__ == '__main__':
     main()
-
-# Explorer https://github.com/Tengrom/Python_nmap/blob/master/README.md
