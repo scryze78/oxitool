8432cff9d4da920ac9616916ffca8c0ce1bcddb0
Ammourk <ammourk@yahoo.fr>
2020-11-13 11:27:23 +0100

    Update scan5.py

--- scan5.py
+++ scan5.py
@@ -7,6 +7,6 @@ import oxi_db
 #appel les fonctions de la base de données oxi_db
 oxi_db.connect_db()
 oxi_db.create_table()
-#appel de la fonction scanfiles et envoi les resultats du scanne
+#appel de la fonction scan_files_envoi_resultats et envoi les resultats du scanne
 scannefichier.scan_files_envoi_resultats()
 oxi_db.db_close()
