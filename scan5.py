c2a55bfffd73d5be6ecfeba6eb51d3c0363fbff4
Oraclle <73529753+Oraclle@users.noreply.github.com>
2020-11-13 11:19:47 +0100

    Add files via upload

---
+++ scan5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# auteur Kamel, Sekou, Josoa
+# date 13/11/2020
+import sannefichier
+import oxi_db
+
+#appel les fonctions de la base de données oxi_db
+oxi_db.connect_db()
+oxi_db.create_table()
+#appel de la fonction scanfiles et envoi les resultats du scanne
+scannefichier.scan_files_envoi_resultats()
+oxi_db.db_close()
