a4f4e87b9223507d24c2d1b86d07f0ad701e0b28
Nyro972 <73656967+Nyro972@users.noreply.github.com>
2020-12-15 05:02:58 -0400

    Update oxitool.py

--- oxitool.py
+++ oxitool.py
@@ -3,16 +3,23 @@
 # fichier: oxitool.py
 # auteur: Rony MOUNIAPIN, Adrien CELLIER
 # entreprise: POP School
-# date: 10/11/2020
+# date: 15/12/2020 --Rony
 
 import scan_vuln
 import oxi_db
 
-#appel des fonctions de connection, creation et fermeture de la base de données
-oxi_db.connect_db()
-oxi_db.create_table()
-oxi_db.db_close()
-#appel de la fonction scan et envoi resultats de la scan_vuln
-scan_vuln.scan_et_envoi_resultats()
-
+#Appel de la fonction create_connection() et permet de récupérer le curseur de connection dans la variable "conn"
+conn = create_connection(r"chemin_sur_la_clé) """en attente"""
+#Si on a réussi à creer la base on fait ce qu'il y a dans le main
+if conn is not None:
+  create_table(conn)
+  #appel des fonctions de connection, creation et fermeture de la base de données
+  oxi_db.connect_db()
+  oxi_db.create_table()
+  oxi_db.db_close()
+  #appel de la fonction scan et envoi resultats de la scan_vuln
+  scan_vuln.scan_et_envoi_resultats()
+#sinon erreur
+else:
+  print ("Erreur! problème de connexion à la base de données")
 
