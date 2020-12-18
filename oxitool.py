d50cbffbab3e17278a2ec764ffd6dce513b24f07
Nyro972 <73656967+Nyro972@users.noreply.github.com>
2020-12-18 06:15:59 -0400

    Update oxitool.py

--- oxitool.py
+++ oxitool.py
@@ -15,9 +15,11 @@ if conn is not None:
   create_table(conn)
   #appel des fonctions de connection, creation et fermeture de la base de données
   oxi_db.create_table(conn)
-  oxi_db.db_close(conn)# à voir si je supprime
   #appel de la fonction scan et envoi resultats de la scan_vuln
   vulnerabilites = scan_vuln.scan_et_envoi_resultats()
+  
+  oxi_db.db_close(conn)# à voir si je supprime
+
 #sinon erreur
 else:
   print ("Erreur! problème de connexion à la base de données")
