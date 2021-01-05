103de00ff7d3521a96c7b65538789b6e5fd39b0a
Nyro972 <73656967+Nyro972@users.noreply.github.com>
2021-01-05 12:35:15 -0400

    Update oxitool.py

--- oxitool.py
+++ oxitool.py
@@ -3,22 +3,28 @@
 # fichier: oxitool.py
 # auteur: Rony MOUNIAPIN, Adrien CELLIER
 # entreprise: POP School
-# date: 18/12/2020 --Rony
-
+# date: 05/01/2021 --Rony
+import scan_file
 import scan_vuln
 import oxi_db
 
+
 #Appel de la fonction create_connection() et permet de récupérer le curseur de connection dans la variable "conn"
-conn = create_connection(r"chemin_sur_la_clé) """en attente"""
+conn = oxi_db.create_connection(r"H:\Sqlite\db\pornhub.db")
+PATH = "C:\\"
+liste_ini = []
 #Si on a réussi à creer la base on fait ce qu'il y a dans le main
 if conn is not None:
-  create_table(conn)
   #appel des fonctions de connection, creation et fermeture de la base de données
   oxi_db.create_table(conn)
+  #appel de la fonction recherche de fichier
+  var_fichier = scan_file.recherche(liste_ini, PATH)
+  #appel de la fonction d'insertion de fichiers
+  oxi_db.insert_file(conn, var_fichier)
   #appel de la fonction scan et envoi resultats de la scan_vuln
   vulnerabilites = scan_vuln.scan_et_envoi_resultats()
-  #appel de la fonction d'insertion
-  insert_port(conn, vulnerabilites)
+  #appel de la fonction d'insertion de vulnérabilités
+  oxi_db.insert_port(conn, vulnerabilites)
   """:param conn pour la connection à la bdd
      :param vulnerabilites tuple des ports et leurs états """
   oxi_db.db_close(conn)# à voir si je supprime
@@ -26,4 +32,3 @@ if conn is not None:
 #sinon erreur
 else:
   print ("Erreur! problème de connexion à la base de données")
-
